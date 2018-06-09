5eae637365bf8edf66e9ee05c376185105cfdd2b
CyberOJ <cyberOJ12@gmail.com>
2018-06-09 16:21:27 -0500

    wooden spoon

--- src/com/cbt/Second.java
+++ src/com/cbt/Second.java
@@ -1,7 +1,18 @@
 package com.cbt;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Second {
 	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", 
+				"C:\\Users\\ojuraev\\CyberTek\\Selenium\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 	
+		driver.get("https://www.msn.com/");
+		
+	}
 	
 }
